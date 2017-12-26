e2cdd73cd02cd3090991a9c6ce9c8effb4730f7a
minimalparts <aurelie.herbelot@cantab.net>
2017-12-26 16:14:29 +0100

    Added crawling to htmlparser.

--- app/indexer/htmlparser.py
+++ app/indexer/htmlparser.py
@@ -1,6 +1,7 @@
 import logging
 import requests
 import justext
+from urllib.parse import urljoin
 from app.indexer import detect_open
 from bs4 import BeautifulSoup
 from langdetect import detect
@@ -13,6 +14,34 @@ def remove_boilerplates(response):
             text+=paragraph.text+" "
     return text
 
+def BS_parse(url):
+    req=None
+    try:
+        req = requests.get(url, allow_redirects=True, timeout=30)
+        req.encoding = 'utf-8'
+    except:
+        print("Request failed when trying to index",url,"...")
+        return False, req
+    if req.status_code is not 200:
+        logging.exception("Warning: "  + str(req.url) + ' has a status code of: ' \
+        + str(req.status_code) + ' omitted from database.\n')
+        return False, req
+    bs_obj = BeautifulSoup(req.text,"lxml")
+    return bs_obj, req
+
+def extract_links(url):
+    links = []
+    bs_obj, req = BS_parse(url)
+    if not bs_obj:
+        return links
+    hrefs = bs_obj.findAll('a', href=True)
+    for h in hrefs:
+        if h['href'].startswith('http'):
+            links.append(h['href'])
+        else:
+            links.append(urljoin(url,h['href']))
+    return links
+
 def extract_from_url(url):
     '''From history info, extract url, title and body of page,
     cleaned with BeautifulSoup'''
@@ -20,19 +49,11 @@ def extract_from_url(url):
     body_str=""
     snippet=""
     cc=False
-    req=None
-    try:
-        req = requests.get(url, allow_redirects=True, timeout=30)
-        req.encoding = 'utf-8'
-    except:
-        print("Request failed when trying to index",url,"...")
+    bs_obj, req = BS_parse(url)
+    if not bs_obj:
+        print("Failed to get BeautifulSoup object...")
         return title, body_str, snippet, cc
-    if req.status_code is not 200:
-        logging.exception("Warning: "  + str(req.url) + ' has a status code of: ' \
-        + str(req.status_code) + ' omitted from database.\n')
-        return title, body_str, snippet, cc
-    bs_obj = BeautifulSoup(req.text,"lxml")
-    if hasattr(bs_obj.title, 'string') & (req.status_code == requests.codes.ok):
+    if hasattr(bs_obj.title, 'string'):
         if url.startswith('http'):
            title = bs_obj.title.string
            if title is None:
@@ -43,6 +64,7 @@ def extract_from_url(url):
                print("Language for",url,":",language)
            except:
                title=""
+               print("Couldn't detect page language.")
                return title, body_str, snippet, cc
                
            if detect(title+" "+body_str) != "en":
