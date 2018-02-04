d3c1a2291d631c1a1c0ed2806b64195390f3881b
minimalparts <aurelie.herbelot@cantab.net>
2018-02-04 18:26:00 +0100

    Added pages module.

---
+++ app/pages/controllers.py
@@ -0,0 +1,15 @@
+# Import flask dependencies
+from flask import Blueprint, render_template
+
+from app.api.models import Pods
+
+# Define the blueprint:
+pages = Blueprint('pages', __name__, url_prefix='')
+
+@pages.route('/faq/')
+def return_faq():
+    return render_template("pages/faq.html")
+
+@pages.route('/acknowledgements/')
+def return_acknowledgements():
+    return render_template("pages/acknowledgements.html")
