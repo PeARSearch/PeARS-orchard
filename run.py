793b5a3b1020fef9d89a0421f1ec71cfd4e3430d
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-09-13 21:34:29 +0530

    fixing bug on earlier commit

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 # Run a test server.
 import zipfile
 import os
-from app import app
+
 if  os.path.exists('app/static/spaces/english.dm')==False:
     with zipfile.ZipFile('app/static/spaces/english.dm.zip', 'r') as zip_ref:
         zip_ref.extractall('app/static/spaces/')
-
+from app import app
 app.run(host='0.0.0.0', port=8080, debug=True)
