7a69c5603c8b2daa718dd33213ed537f4c20f92b
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-09-13 15:39:21 +0530

    extracting zip files could be done in code itself
    The extraction of the zip file can take place inside the code and no need of manual extraction.This would also be helpful if this project is further made into a pip package.The extraction would not happen if the file is already there.

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Run a test server.
+import zipfile
+import os
 from app import app
+if  os.path.exists('app/static/spaces/english.dm')==False:
+    with zipfile.ZipFile('app/static/spaces/english.dm.zip', 'r') as zip_ref:
+        zip_ref.extractall('app/static/spaces/')
+
 app.run(host='0.0.0.0', port=8080, debug=True)
