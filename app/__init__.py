2f44b9cee046934141333aa538bda21c3fd327cc
stultus <hrishi.kb@gmail.com>
2022-10-12 22:49:49 +0530

    Basic cleanup on search/controllers

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,21 @@
+# import logging 
+import logging
+
 # Import flask and template operators
 from flask import Flask, render_template
 from flask_admin import Admin
 
 # Import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
+
+
+def configure_logging():
+    # register root logging
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('werkzeug').setLevel(logging.INFO)
+
+
+configure_logging()
 
 # Define the WSGI application object
 app = Flask(__name__)
@@ -15,10 +27,6 @@ app.config.from_object('config')
 # by modules and controllers
 db = SQLAlchemy(app)
 
-# Sample HTTP error handling
-#@app.errorhandler(404)
-#def not_found(error):
-#    return render_template('404.html'), 404
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 from app.indexer.controllers import indexer as indexer_module
--- app/search/controllers.py
+++ app/search/controllers.py
@@ -1,5 +1,6 @@
 # Import flask dependencies
 from flask import Blueprint, request, render_template, send_from_directory
+from flask import current_app
 
 # Import the database object from the main app module
 from app.api.models import Urls
@@ -7,38 +8,27 @@ from app.search import score_pages
 
 # Import utilities
 import re
+import logging
+
+
+LOG = logging.getLogger(__name__)
 
 # Define the blueprint:
 search = Blueprint('search', __name__, url_prefix='')
 
 
-def get_cached_urls(urls):
-    urls_with_cache = urls
-    for u in urls_with_cache:
-        cache = re.sub(r"http\:\/\/|https\:\/\/", "", u[0])
-        if cache[-5:] != ".html":
-            cache += ".html"
-        print(cache)
-        u.append(cache)
-    return urls_with_cache
-
-
 @search.route('/')
 @search.route('/index')
-def index():
-    '''Check whether DS has been updated.'''
-    # if DS_M.shape[0] != len(Urls.query.all()):
-    #    mk_matrix_from_db()
+def index():  
     results = []
     internal_message = ""
-    if len(Urls.query.all()) == 0:
+    if Urls.query.count() == 0:
         internal_message = "Hey there! It looks like you're here\
          for the first time :) To understand how to use PeARS,\
          go to the FAQ (link at the top of the page)."
     query = request.args.get('q')
-    print(request, request.args, query)
     if not query:
-        print("no query")
+        LOG.info("No Querry")
         return render_template(
             "search/index.html",
             internal_message=internal_message)
@@ -51,7 +41,6 @@ def index():
             pears = ['no pear found :(']
             score_pages.ddg_redirect(query)
 
-        # results = get_cached_urls(results)
         return render_template(
             'search/results.html', pears=pods, query=query, results=results)
 
